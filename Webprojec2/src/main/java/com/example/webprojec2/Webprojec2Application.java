5f235c8d3ca61c87620b9ca42ccb876f31f1c6f6
LeeKangJun33 <62988674+LeeKangJun33@users.noreply.github.com>
2021-05-18 22:34:47 +0900

    반응형버전

---
+++ Webprojec2/src/main/java/com/example/webprojec2/Webprojec2Application.java
@@ -0,0 +1,13 @@
+package com.example.webprojec2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Webprojec2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Webprojec2Application.class, args);
+    }
+
+}
---
+++ Webprojec2/src/test/java/com/example/webprojec2/Webprojec2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webprojec2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Webprojec2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
