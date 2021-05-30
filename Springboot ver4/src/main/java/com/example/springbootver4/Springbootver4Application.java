df1d419cc0990a7dc0fb26c409c21b6b9640dd64
LeeKangJun33 <62988674+LeeKangJun33@users.noreply.github.com>
2021-05-31 00:17:36 +0900

    springboot pom.xml로 재구성함

---
+++ Springboot ver4/src/main/java/com/example/springbootver4/Springbootver4Application.java
@@ -0,0 +1,13 @@
+package com.example.springbootver4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springbootver4Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Springbootver4Application.class, args);
+    }
+
+}
